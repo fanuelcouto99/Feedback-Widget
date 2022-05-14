ac2159e9e63ac26e1816b06bb2dbe7f9357a3d19
Fanuel Couto <fanuel.couto99@gmail.com>
2022-05-13 22:56:11 -0300

    Finalizando backend da aplicação, configurado envio da imagem por email

--- server/src/use-cases/submit-feedback-use-case.ts
+++ server/src/use-cases/submit-feedback-use-case.ts
@@ -16,15 +16,15 @@ export class SubmitFeedbackUseCase {
     async execute(request: SubmitFeedbackUseCaseRequest) {
         const { type, comment, screenshot } = request;
 
-        if(!type) {
+        if (!type) {
             throw new Error('Type is required.');
         }
 
-        if(!comment) {
+        if (!comment) {
             throw new Error('Type is required.');
         }
 
-        if(screenshot && !screenshot.startsWith('data:image/png;base64')) {
+        if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
             throw new Error('Invalid screenshot format.');
         }
 
@@ -32,12 +32,13 @@ export class SubmitFeedbackUseCase {
             type, comment, screenshot
         });
 
-        await this.mailAdapter.sendMail({ 
+        await this.mailAdapter.sendMail({
             subject: 'Novo feedback',
             body: [
                 `<div style="font-family: sans-serif; font-size: 16px; color: #111;">`,
                 `<p> Tipo do feedback: ${type}</p>`,
                 `<p> Comentário: ${comment}</p>`,
+                screenshot ? `<img src="${screenshot}"/>` :  ``,
                 `</div>`
             ].join('\n')
         });
