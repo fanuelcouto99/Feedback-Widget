e87a9f042edbe8f26e7e2e4e3a58c91c11b7fdae
Fanuel Couto <fanuel.couto99@gmail.com>
2022-05-13 22:56:58 -0300

    Configurado loading no botão ao enviar feedback e feita integração com backend

--- web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ web/src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -1,26 +1,37 @@
 import { ArrowLeft, Camera } from "phosphor-react";
 import { FormEvent, useState } from "react";
 import { FeedbackType, feedbackTypes } from "..";
+import { api } from "../../../lib/api";
 import { CloseButton } from "../../CloseButton";
-import { ScreenshotButton } from "../ScreenShotButton";
+import { Loading } from "../../Loading";
+import { ScreenshotButton } from "../ScreenshotButton";
 
 interface FeedbackContentStepProps {
     feedbackType: FeedbackType;
     onFeedbackRestartRequested: () => void;
     onFeedbackSent: () => void;
-}
+};
 
 export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, onFeedbackSent }: FeedbackContentStepProps) {
 
     const feedbackTypeInfo = feedbackTypes[feedbackType];
     const [screenshot, setScreenshot] = useState<string | null>(null);
     const [comment, setComment] = useState('');
+    const [isSendingFeedback, setIsSendingFeedback] = useState(false);
 
-    function handleSubmitFeedback(event: FormEvent) {
+    async function handleSubmitFeedback(event: FormEvent) {
         event.preventDefault();
-        console.log({screenshot, comment})
+
+        setIsSendingFeedback(true);
+
+        await api.post('/feedbacks', {
+            type: feedbackType,
+            comment,
+            screenshot
+        });
+
         onFeedbackSent();
-    }
+    };
 
     return (
         // Sinal de <> é usado para criar um fragmento (div) que não é exibida no html final da aplicação
@@ -45,8 +56,8 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
 
             <form onSubmit={handleSubmitFeedback} className="my-4 w-full">
                 <textarea className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-none scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
-                    placeholder="Conte com detalhes o que está acontecendo..." 
-                    onChange={event => setComment(event.target.value)}/>
+                    placeholder="Conte com detalhes o que está acontecendo..."
+                    onChange={event => setComment(event.target.value)} />
 
                 <footer className="flex gap-2 mt-2">
                     <ScreenshotButton
@@ -56,10 +67,10 @@ export function FeedbackContentStep({ feedbackType, onFeedbackRestartRequested, 
 
                     <button
                         type="submit"
-                        disabled={comment.length === 0}
+                        disabled={comment.length === 0 || isSendingFeedback}
                         className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors disabled:opacity-50 disabled:hover:bg-brand-500"
                     >
-                        Enviar feedback
+                        {isSendingFeedback ? <Loading /> : 'Enviar feedback'}
                     </button>
                 </footer>
             </form>
